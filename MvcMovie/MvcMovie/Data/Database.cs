e70072551cac876716654e61d3577fe73393e2b3
SuhaibALbaradan <114123798+SuhaibALbaradan@users.noreply.github.com>
2024-04-12 23:40:47 -0400

    Create Database.cs

---
+++ MvcMovie/MvcMovie/Data/Database.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace suhaib
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Database access has already been done on April 14, 2024 by suhaib");
+        }
+    }
+}
